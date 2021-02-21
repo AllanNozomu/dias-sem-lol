3c210e13c2b2fd6e9645701c94a4ec228c9f80b8
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2021-02-21 18:26:00 -0300

    Updates

---
+++ precache-manifest.c8f45c399c364a6b339a7b4dceff6732.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/dias-sem-lol/favicon.ico"
+  },
+  {
+    "revision": "31b9415a3adbbfc22b8e4822460a52c7",
+    "url": "/dias-sem-lol/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/dias-sem-lol/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/dias-sem-lol/manifest.json"
+  },
+  {
+    "revision": "c9cbeaea30d820f223cc",
+    "url": "/dias-sem-lol/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "c9cbeaea30d820f223cc",
+    "url": "/dias-sem-lol/static/js/main.c9cbeaea.chunk.js"
+  },
+  {
+    "revision": "ba6e1c08d999cfcfca94",
+    "url": "/dias-sem-lol/static/js/runtime~main.ba6e1c08.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/dias-sem-lol/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
